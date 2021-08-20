697c4122c462b54e2023501ad8c63c12e68e5a49
onesis <Onesis.olea@gmail.com>
2021-08-20 18:02:49 -0400

    refactor calculator buttons to be a function using hooks

--- src/components/Calbuttons.js
+++ src/components/Calbuttons.js
@@ -1,20 +1,13 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
 
-class Buttons extends React.Component {
-  constructor(props) {
-    super(props);
-    this.btnClicked = props.btnClicked;
-  }
-
-  render() {
-    const buttons = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
-    return (
-      <div className="buttons">
-        {buttons.map((btn) => <button key={btn} type="button" onClick={this.btnClicked}>{btn}</button>)}
-      </div>
-    );
-  }
-}
+const Buttons = (props) => {
+  const { onClick } = props;
+  const buttons = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+  return (
+    <div className="buttons">
+      {buttons.map((btn) => <button key={btn} type="button" onClick={(e) => onClick(e)}>{btn}</button>)}
+    </div>
+  );
+};
 
 export default Buttons;
