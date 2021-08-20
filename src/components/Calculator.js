3e1ff599641b28f6883a3de3a6b336b6b53c6cfa
onesis <Onesis.olea@gmail.com>
2021-08-20 03:36:04 -0400

    Add event handler

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,15 +1,40 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-
-const buttons = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      operation: null,
+      next: null,
+      total: null,
+    };
+    this.btnClicked = this.btnClicked.bind(this);
+  }
+
+  btnClicked = (event) => {
+    const buttonName = event.target.innerText;
+    const result = calculate(this.state, buttonName);
+    this.setState((previousState) => {
+      const { operation, next, total } = previousState;
+      return {
+        operation: ((result.operation === undefined) ? operation : result.operation),
+        next: ((result.next === undefined) ? next : result.next),
+        total: ((result.total === undefined) ? total : result.total),
+      };
+    });
+  };
+
   render() {
+    const buttons = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+    const { operation, next, total } = this.state;
+    const display = (total || '') + (operation || '') + (next || '');
     return (
       <div className="calculator">
-        <p className="result">0</p>
+        <p className="result">{display}</p>
         <div className="buttons">
-          {buttons.map((btn) => <button key={btn} type="button">{btn}</button>)}
+          {buttons.map((btn) => <button key={btn} type="button" onClick={this.btnClicked}>{btn}</button>)}
         </div>
       </div>
     );
