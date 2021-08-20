bf85b772c17e7bc539ff7de25be721ba85b812ee
onesis <Onesis.olea@gmail.com>
2021-08-20 18:01:41 -0400

    refactor app.js to be function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  render() {
-    return <Calculator />;
-  }
-}
+const App = () => <Calculator />;
 
 export default App;
