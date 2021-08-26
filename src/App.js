b0666e6d535295963acac768c6bdfc81f4a28dff
onesis <Onesis.olea@gmail.com>
2021-08-26 00:36:53 -0400

    Update App.js to use router.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import './App.css';
-import Calculator from './components/Calculator';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import CalculatorPage from './pages/CalculatorPage';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Quote from './pages/Quote';
 
-const App = () => <Calculator />;
+const App = () => (
+  <Router>
+    <Header />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/calculator">
+        <CalculatorPage />
+      </Route>
+      <Route path="/quote">
+        <Quote />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
